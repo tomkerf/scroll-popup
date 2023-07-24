ce01bea1b8e0227a235df6e57d936396d158b5ba
tomkerf <tomkerf@gmail.com>
2023-07-24 13:51:37 +0200

    1er envoi

---
+++ script.js
@@ -0,0 +1,37 @@
+// Créer un événement au scroll
+// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+// Faire apparaitre l'image de la partie improvise
+// Faire apparaitre la popup quand on est en bas du site
+// Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+let playOnce = true;
+
+window.addEventListener("scroll", () => {
+  // Navbar effect
+  if (window.scrollY > 50) {
+    navbar.style.height = 45 + "px";
+  } else {
+    navbar.style.height = 90 + "px";
+  }
+
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+  // Image
+  if (scrollValue > 0.45) {
+    imgImprovise.style.opacity = "1";
+    imgImprovise.style.transform = "none";
+  }
+
+  // Popup
+  if (scrollValue > 0.85 && playOnce) {
+    popup.style.opacity = "1";
+    popup.style.transform = "none";
+    playOnce = false;
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = "0";
+  popup.style.transform = "translateX(500px)";
+});
